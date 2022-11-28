a25c75a8e12631dab2c766a6778f8ec8bd2aeaf3
waseemkhalifa <whkhalifa@gmail.com>
2022-11-28 04:21:57 +0000

    folder clean up

---
+++ Python Programs/Simple tic tac toe game.py
@@ -0,0 +1,170 @@
+# This is a simple Create a Tic Tac Toe game, which can be played via the
+# terminal, using the following command in the file location:
+# python3 "Simple tic tac toe game.py"
+
+# Info about the game:
+  # > 2 players will be able to play the game 
+      # (both sitting at the same computer)
+  # > The board will be printed out every time a player makes a move
+  # > You will be able to accept input of the player position which will then  
+      # place a symbol on the board
+
+                                                                      
+# we'll create an empty board using numpy
+import numpy as np
+
+# this function will allow the user to choose the element in which they'd like
+# to add their input
+def choice():
+    # this original choice value can be anything that isn't an integer
+    choice = 'wrong'
+    # while the choice is not a digit, keep asking for input.
+    while choice not in ['1', '2', '3', '4', '5', '6', '7', '8', '9']:
+        # we shouldn't convert here, otherwise we get an error on a wrong
+        # input
+        choice = input('Your Turn, choose an element: ')
+        if choice not in ['1', '2', '3', '4', '5', '6', '7', '8', '9']:
+            print('Sorry, but you did not choose a valid element')
+    # we can convert once the while loop above has confirmed we have a digit
+    choice = int(choice)
+    # this will match the element to the row & column of the array
+    array_position = []
+    if choice == 1:
+       array_position = [0,0]
+    elif choice == 2:
+       array_position = [0,1]
+    elif choice == 3:
+       array_position = [0,2]
+    elif choice == 4:
+       array_position = [1,0]
+    elif choice == 5:
+       array_position = [1,1]
+    elif choice == 6:
+       array_position = [1,2]
+    elif choice == 7:
+       array_position = [2,0]
+    elif choice == 8:
+       array_position = [2,1]
+    elif choice == 9:
+       array_position = [2,2]
+    return array_position
+
+# this is for player_x
+# this function replaces the board with the players input
+def x_choice(board_display, element_choice):
+    row_input = element_choice[0]
+    col_input = element_choice[1]
+    filled_element = ['x', 'o']
+    while board_display[row_input][col_input] in filled_element:
+      print('There is already an input on the board, choose another element')
+      element_choice = choice()
+      row_input = element_choice[0]
+      col_input = element_choice[1]
+    board_display[row_input][col_input] = 'x'
+
+# this is for player_o
+# this function replaces the board with the players input
+def o_choice(board_display, element_choice):
+    row_input = element_choice[0]
+    col_input = element_choice[1]
+    filled_element = ['x', 'o']
+    while board_display[row_input][col_input] in filled_element:
+      print('There is already an input on the board, choose another element')
+      element_choice = choice()
+      row_input = element_choice[0]
+      col_input = element_choice[1]
+    board_display[row_input][col_input] = 'o'
+
+# this function will determine if a player has won
+def won_game(board):
+    # player x
+    if board[0][0] == 'x' and board[0][1] == 'x' and board[0][2] == 'x':
+      return True
+    elif board[1][0] == 'x' and board[1][1] == 'x' and board[1][2] == 'x':
+      return True
+    elif board[2][0] == 'x' and board[2][1] == 'x' and board[2][2] == 'x':
+      return True
+    elif board[0][0] == 'x' and board[1][0] == 'x' and board[2][0] == 'x':
+      return True
+    elif board[0][1] == 'x' and board[1][1] == 'x' and board[2][1] == 'x':
+      return True
+    elif board[0][2] == 'x' and board[1][2] == 'x' and board[2][2] == 'x':
+      return True
+    elif board[0][0] == 'x' and board[1][1] == 'x' and board[2][2] == 'x':
+      return True
+    elif board[0][2] == 'x' and board[1][1] == 'x' and board[2][0] == 'x':
+      return True
+    # player o
+    elif board[0][0] == 'o' and board[0][1] == 'o' and board[0][2] == 'o':
+      return True
+    elif board[1][0] == 'o' and board[1][1] == 'o' and board[1][2] == 'o':
+      return True
+    elif board[2][0] == 'o' and board[2][1] == 'o' and board[2][2] == 'o':
+      return True
+    elif board[0][0] == 'o' and board[1][0] == 'o' and board[2][0] == 'o':
+      return True
+    elif board[0][1] == 'o' and board[1][1] == 'o' and board[2][1] == 'o':
+      return True
+    elif board[0][2] == 'o' and board[1][2] == 'o' and board[2][2] == 'o':
+      return True
+    elif board[0][0] == 'o' and board[1][1] == 'o' and board[2][2] == 'o':
+      return True
+    elif board[0][2] == 'o' and board[1][1] == 'o' and board[2][0] == 'o':
+      return True
+
+# this is our game
+def play_tic_tac_toe():
+    # this will build our empty board 
+    board = np.array([['1','2','3'],['4','5','6'],['7','8','9']])
+    # this will be a counter of how many turns we've had in the game
+    turns = 0
+    # this is the max turns the game can have
+    max_turns = 9
+    # players in the game
+    players = ['x', 'o']
+    # while loop, to loop while the number of turns is less/equal to max_turns
+    while turns < max_turns:
+        # for loop for numbers of players
+        for p in players:
+          # we'll print the board at the start of every turn
+          print(board)
+          # break out of for loop if we've played all turns
+          if turns == max_turns:
+             print('Nobody wins!')
+             break
+          if turns > max_turns:
+             break
+          elif p == 'x':
+            print('You are player x')
+            # user inputs in which element they'd like their input
+            element_choice = choice()
+            # this will input the user's choice on the board
+            # it will also take care of wrong inputs
+            x_choice(board, element_choice)
+            # we'll increment by one at the end of each turn
+            turns+=1
+            # check to see if player x has won
+            if won_game(board) == True:
+               print('Player X has won!')
+               print(board)
+               turns = max_turns + 1
+               break
+          # player o
+          elif p == 'o':
+            print('You are player o')
+            # user inputs in which element they'd like their input
+            element_choice = choice()
+            # this will input the user's choice on the board
+            # it will also take care of wrong inputs
+            o_choice(board, element_choice)
+            # we'll increment by one at the end of each turn
+            turns+=1
+            # check to see if player o has won
+            if won_game(board) == True:
+               print('Player O has won!')
+               print(board)
+               turns = max_turns + 1
+               break
+
+# to play the game
+play_tic_tac_toe()
