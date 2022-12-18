94d7f3a2163b9842a1faefb81b0d5d6f04c41b4e
waseemkhalifa <whkhalifa@gmail.com>
2022-12-18 20:30:36 +0000

    added comments at the start of script

--- Python Programs/Text based blackjack game/text based blackjack game.py
+++ Python Programs/Text based blackjack game/text based blackjack game.py
@@ -1,7 +1,26 @@
 # ---------------------------------------------------------------------------                                                                           
 # Text based blackjack game
 # ---------------------------------------------------------------------------                                                                         
-# 
+# this is a simple text based blackjack game coded in python
+# the game can be played via the terminal, using the following command 
+# in the file location:
+# python3 "filename.py"
+
+# the player will be asked to bet from their bank (initially value of £100)
+# the player will be shown two cards
+# the dealers 1 card out of their two will be shown
+# the player will be given a choice to hit or stand
+# once the player stands, the dealer will continue to hit until their
+# card value is above the players
+# whoever gets closest to 21 wins
+# whoever gets 21 wins (Blackjack)
+# a draw will occur if both players have the same hand value
+# whoever hits and their hand goes above 21, they go bust and the other
+# player wins
+# at the end of the round the players bank either increases or decreases
+# based on their bet, and the outcome of the round (win or lose)
+# at the end of each round the player can choose to continue playing
+# or to quit
 
 # this will be used to shuffle the deck
 import random
