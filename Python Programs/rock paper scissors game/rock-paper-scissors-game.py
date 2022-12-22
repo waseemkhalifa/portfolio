a3d35159087286f5d2b5fec48d0c090d080576b7
waseemkhalifa <whkhalifa@gmail.com>
2022-12-22 23:42:21 +0000

    strated work on rock paper scissors game

---
+++ Python Programs/rock paper scissors game/rock-paper-scissors-game.py
@@ -0,0 +1,9 @@
+# ---------------------------------------------------------------------------                                                                           
+# Text based rock paper scissors game
+# ---------------------------------------------------------------------------
+# this is a simple text based rock paper scissors game coded in python
+# the game can be played via the terminal, using the following command 
+# in the file location:
+# python3 "filename.py"
+
+# the 
