116877dda4a617510bdf28a52c52cadae17df143
waseemkhalifa <whkhalifa@gmail.com>
2023-02-19 12:44:10 +0000

    created won_lost fn

--- Rust Programs/rock-paper-scissors-game/rock-paper-scissors/src/main.rs
+++ Rust Programs/rock-paper-scissors-game/rock-paper-scissors/src/main.rs
@@ -22,18 +22,17 @@ fn main() {
     hm_choice.insert(2, "Paper");
     hm_choice.insert(3, "Scissors");
     println!("{:?}", hm_choice);
-
-    // this prints random values
-    let mut rng = rand::thread_rng();
-    println!("Random Value: {}", rng.gen_range(1..4));
+    println!("");
 
     // default scores for the player & computer
     // we'll first define the score variables
-    let mut player_score = 0;
-    let mut computer_score = 0;  
+    // let mut player_score = 0;
+    // let mut computer_score = 0;  
 
-    // players_choice();
-    let test = computer_choice();
+
+    let player_chosen1: &str = "Rock";
+    let computer_chosen1: &str = "Scissors";
+    let test = won_lost(&player_chosen1, &computer_chosen1);
     println!("{}", test);
 }
 
@@ -55,23 +54,32 @@ use std::io;
 // to our hashmap
 fn players_choice() -> i32 {
 
+    // the only values we'll accept for our user input
     let correct_input: [i32; 3] = [1, 2, 3];
 
+    // this is a loop, which will only end if a valid input is entered
     let mut choice = loop {
         println!("Choose one of the following:");
         println!("Rock(1), Paper(2) or Scissors(3)");
+
+        // this is Rust's user input method
 		let mut choice = String::new();
 		io::stdin().read_line(&mut choice).unwrap();
+
+        // if the value entered was int
 		if let Ok(val) = choice.trim().parse::<i32>() {
             if correct_input.contains(&val) {
                 break val;
             }
+            // this will bring if the value wasn't our valid input values
             println!("");
             println!("Wrong Input!");
             println!("Only 1, 2 or 3 are valid inputs");
             println!("");
 			continue;
 		}
+
+        // this will be printed if the value entered was not int
         println!("");
         println!("Wrong Input!");
         println!("Only 1, 2 or 3 are valid inputs");
@@ -82,7 +90,33 @@ fn players_choice() -> i32 {
 
 // this function will return the computer's choice (at random)
 fn computer_choice() -> i32 {
-    // this prints random values between 1 and 3
+    // this retuns a  random value between 1 and 3
     let choice: i32 = rand::thread_rng().gen_range(1..4);
     return choice;
+}
+
+// this function will work who has won or lost the round
+fn won_lost<'a>(player_chosen: &'a str, computer_chosen: &'a str) -> &'a str {
+
+    let mut return_value: &str = "";
+
+    // if player and computer have the same choice
+    if player_chosen == computer_chosen {
+        return_value = "DRAW!";
+    // player wins
+    } else if player_chosen == "Rock" && computer_chosen == "Scissors" {
+        return_value = "YOU WIN!";
+    } else if player_chosen == "Paper" && computer_chosen == "Rock" {
+        return_value = "YOU WIN!";
+    } else if player_chosen == "Scissors" && computer_chosen == "Paper" {
+        return_value = "YOU WIN!";
+    // computer wins
+    } else if computer_chosen == "Rock" && player_chosen == "Scissors" {
+        return_value = "YOU LOSE!";
+    } else if computer_chosen == "Paper" && player_chosen == "Rock" {
+        return_value = "YOU LOSE!";
+    } else if computer_chosen == "Scissors" && player_chosen == "Paper" {
+        return_value = "YOU LOSE!";
+    }
+    return return_value;
 }
