aad45ecbe390f9726c2300c959087840a28bf609
waseemkhalifa <whkhalifa@gmail.com>
2023-02-23 17:40:32 +0000

    built out main

--- Rust Programs/tic-tac-toe-game/tic-tac-toe/src/main.rs
+++ Rust Programs/tic-tac-toe-game/tic-tac-toe/src/main.rs
@@ -12,30 +12,47 @@
 
 /* ----------------------- main ----------------------- */
 fn main() {
+    // we'll create a hashmap to map int to array element
+    let mut hm_choice: HashMap<i32, [i32;2]> = HashMap::new();
+    hm_choice.insert(1, [0,0]);
+    hm_choice.insert(2, [0,1]);
+    hm_choice.insert(3, [0,2]);
+    hm_choice.insert(4, [1,0]);
+    hm_choice.insert(5, [1,1]);
+    hm_choice.insert(6, [1,2]);
+    hm_choice.insert(7, [2,0]);
+    hm_choice.insert(8, [2,1]);
+    hm_choice.insert(9, [2,2]);
+
     // this will build our empty board 
     let mut board = arr2(&[[1, 2, 3], [4, 5, 6], [7, 8, 9]]);
-    // this will be a counter of how many turns we"ve had in the game
+    // this will be a counter of how many turns we've had in the game
     let mut turns = 0;
     // this is the max turns the game can have
     let max_turns = 9;
     // players in the game
-    let players = ["x", "o"];
+    let players = ["X", "O"];
+    
     // while loop, to loop while the number of turns is less/equal to max_turns
     while turns < max_turns {
         // for loop for numbers of players
-        for p in players:
+        for p in players {
             // we"ll println! the board at the start of every turn
-            println!("{}", board);
+            println!("");
+            println!("{:?}", board);
+            println!("");
             // break out of for loop if we"ve played all turns
             if turns == max_turns {
+                println!("");
                 println!("Nobody wins!");
                 break;
             } else if turns > max_turns {
                 break;
-            } else if p == "x" {
-                println!("You are player x");
-                // user inputs in which element they"d like their input
-                element_choice = choice();
+            } else if p == "X" {
+                println!("You are player X");
+                // user inputs in which element they'd like their input
+                let element_choice = choice();
+                let element_choice = hm_choice.get(&element_choice).unwrap();
                 // this will input the user"s choice on the board
                 // it will also take care of wrong inputs
                 x_choice(board, element_choice);
@@ -43,17 +60,21 @@ fn main() {
                 turns+=1;
                 // check to see if player x has won
                 if won_game(board) == True {
+                    println!("");
+                    println!("{:?}", board);
+                    println!("");
                     println!("Player X has won!");
-                    println!(board);
+                    println!("");
                     turns = max_turns + 1;
                     break;
                 }
             }
             // player o
-            else if p == "o" {
-                println!("You are player o");
+            else if p == "O" {
+                println!("You are player O");
                 // user inputs in which element they"d like their input
-                element_choice = choice();
+                let element_choice = choice();
+                let element_choice = hm_choice.get(&element_choice).unwrap();
                 // this will input the user"s choice on the board
                 // it will also take care of wrong inputs
                 o_choice(board, element_choice);
@@ -61,53 +82,61 @@ fn main() {
                 turns+=1;
                 // check to see if player o has won
                 if won_game(board) == True {
+                    println!("");
+                    println!("{:?}", board);
+                    println!("");
                     println!("Player O has won!");
-                    println!(board);
+                    println!("");
                     turns = max_turns + 1;
                     break;
                 }
             }
+        }
     }
 }
 
 /* ----------------------- packages ----------------------- */
 // this will be used by the computer to choose rock paper scissors at random
 use ndarray::arr2;
+// we'll use this to map the user's input to the element in the array
+use std::collections::HashMap;
+// this will allow the user to input their choice for the game
+use std::io;
 
 /* ----------------------- functions ----------------------- */
 // this function will allow the user to choose the element in which they"d like
 // to add their input
-def choice():
+fn choice() -> i32 {
     // the only values we'll accept for our user input
-    let correct_input: [i32; 9] = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    let correct_input: [i32; 9] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-    // while the choice is not a digit, keep asking for input.
-    while choice not in ['1', '2', '3', '4', '5', '6', '7', '8', '9']:
-        // we shouldn't convert here, otherwise we get an error on a wrong
-        // input
-        choice = input('Your Turn, choose an element: ');
-        if choice not in ['1', '2', '3', '4', '5', '6', '7', '8', '9']:
-            print('Sorry, but you did not choose a valid element')
-    // we can convert once the while loop above has confirmed we have a digit
-    choice = int(choice)
-    // this will match the element to the row & column of the array
-    array_position = []
-    if choice == 1:
-        array_position = [0,0];
-    elif choice == 2:
-       array_position = [0,1];
-    elif choice == 3:
-       array_position = [0,2];
-    elif choice == 4:
-        array_position = [1,0];
-    elif choice == 5:
-       array_position = [1,1];
-    elif choice == 6:
-       array_position = [1,2];
-    elif choice == 7:
-       array_position = [2,0];
-    elif choice == 8:
-       array_position = [2,1];
-    elif choice == 9:
-       array_position = [2,2];
-    return array_position
+    // this is a loop, which will only end if a valid input is entered
+    let choice = loop {
+        println!("");
+        println!("Your Turn, choose an element: ");
+
+        // this is Rust's user input method
+		let mut choice = String::new();
+		io::stdin().read_line(&mut choice).unwrap();
+
+        // if the value entered was int
+		if let Ok(val) = choice.trim().parse::<i32>() {
+            if correct_input.contains(&val) {
+                break val;
+            }
+            // this will bring if the value wasn't our valid input values
+            println!("");
+            println!("Wrong Input!");
+            println!("Sorry, but you did not choose a valid element");
+			continue;
+		}
+
+        // this will be printed if the value entered was not int
+        println!("");
+        println!("Wrong Input!");
+        println!("Sorry, but you did not choose a valid element");
+	};
+     
+    return choice;
+}
+
