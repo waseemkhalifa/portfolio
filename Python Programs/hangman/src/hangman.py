073b7a388cf11b55d897ffd9b7cd609d9896641c
waseemkhalifa <whkhalifa@gmail.com>
2023-08-25 21:06:15 +0100

    created project files

---
+++ Python Programs/hangman/src/hangman.py
@@ -0,0 +1,24 @@
+# ------------------------------------ functions --------------------------- #
+
+# this function will return a word for the player to guess
+def get_word():
+    pass 
+
+# this function will replace the word with dashes
+def dash_word():
+    pass
+
+# this function will print out the hangman
+
+
+
+
+
+
+
+
+
+
+
+
+
