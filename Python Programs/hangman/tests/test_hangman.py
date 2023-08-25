49d70f1ae4d5e1ef6df4929a669f38bfe6a6e552
waseemkhalifa <whkhalifa@gmail.com>
2023-08-25 21:43:59 +0100

    added more tests

--- Python Programs/hangman/tests/test_hangman.py
+++ Python Programs/hangman/tests/test_hangman.py
@@ -14,9 +14,21 @@ def dash_word(word):
 class word_frequency_finder_tests(unittest.TestCase):
 
     # test for dash_word function
-    def test_dash_word(self):
+    def test_dash_word_hello(self):
         self.test_input = "hello"
         self.test_expected = "_ _ _ _ _"
+        self.test_output = dash_word(self.test_input)
+        self.assertEqual(self.test_output, self.test_expected)
+    
+    def test_dash_word_wave(self):
+        self.test_input = "wave"
+        self.test_expected = "_ _ _ _"
+        self.test_output = dash_word(self.test_input)
+        self.assertEqual(self.test_output, self.test_expected)
+
+    def test_dash_word_a(self):
+        self.test_input = "a!"
+        self.test_expected = "_ _"
         self.test_output = dash_word(self.test_input)
         self.assertEqual(self.test_output, self.test_expected)
 
