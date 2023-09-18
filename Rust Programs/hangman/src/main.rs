26914d8acb67a3cdc0b8c09bea9b5a8f9af847f6
waseemkhalifa <whkhalifa@gmail.com>
2023-09-18 09:24:01 +0100

    continued attempt for fn import_words

--- Rust Programs/hangman/src/main.rs
+++ Rust Programs/hangman/src/main.rs
@@ -1,14 +1,11 @@
-use std::fs::File;
-use std::io::Read;
+/* ----------------------- imports ----------------------- */
+mod words;
+use crate::words::import_words;
+
+/* ----------------------- functions ----------------------- */
 
 fn main() {
     let mut vec_data = Vec::new();
-    let mut f = File::open("src/words.txt").expect("Unable to open file");
-    f.read_to_end(&mut vec_data).expect("Unable to read data");
+    let vec_data = import_words("src/words.txt");
 
-    println!("");
-
-    let string = String::from_utf8(vec_data).expect("Our bytes should be valid utf8");
-
-    println!("{string}")
 }
--- Rust Programs/hangman/src/words.rs
+++ Rust Programs/hangman/src/words.rs
@@ -4,19 +4,24 @@
     we'll have a function to convert the bytes to string format
 */ 
 
-
 /* ----------------------- imports ----------------------- */
 use std::fs::File;
 use std::io::Read;
 
 /* ----------------------- functions ----------------------- */
 
-fn import_words(file_name:string) {
-    let mut vec_data = Vec::new();
+pub fn import_words(file_path:&str) {
+
+    let mut vec_data: Vec<u8> = Vec::new();
     let mut f = File::open(file_path).expect("Unable to open file");
     f.read_to_end(&mut vec_data).expect("Unable to read data");
+
 }
 
+// pub fn bytes_to_string(word:&str, vec_data:Vec) -> &str {
 
+//     let word_stringed = String::from_utf8(vec_data).
+//         expect("Our bytes should be valid utf8");
 
-
+//     return word_stringed;
+// }
