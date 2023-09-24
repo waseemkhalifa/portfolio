8044f6930b38fb7bde1392673ba9be627c283b38
waseemkhalifa <whkhalifa@gmail.com>
2023-09-24 13:48:25 +0100

    completed tests

--- Python Programs/Letter Counter App/tests/test_letter_counter_app.py
+++ Python Programs/Letter Counter App/tests/test_letter_counter_app.py
@@ -12,6 +12,7 @@ class letter_counter_app_tests(unittest.TestCase):
     # test 1
     def test_1(self):
         self.name = "waseem"
+        self.name = lca.title_whitespace(self.name)
 
         self.message = "hello"
 
@@ -23,13 +24,14 @@ class letter_counter_app_tests(unittest.TestCase):
         self.test_output = lca.app_output(self.name, 
                                           self.letter,
                                           self.count_of_letters)
-        self.test_expected = "waseem, your message has 2 l's in it."
+        self.test_expected = "Waseem, your message has 2 l's in it."
         self.assertEqual(self.test_output, self.test_expected)
     
 
     # test 2
     def test_2_zero(self):
-        self.name = "waseem"
+        self.name = " waseem    "
+        self.name = lca.title_whitespace(self.name)
 
         self.message = "hello"
 
@@ -41,13 +43,14 @@ class letter_counter_app_tests(unittest.TestCase):
         self.test_output = lca.app_output(self.name, 
                                           self.letter,
                                           self.count_of_letters)
-        self.test_expected = "waseem, your message has 0 x's in it."
+        self.test_expected = "Waseem, your message has 0 x's in it."
         self.assertEqual(self.test_output, self.test_expected)
 
 
     # test 3
     def test_3_capital(self):
-        self.name = "waseem"
+        self.name = "wasEEEm"
+        self.name = lca.title_whitespace(self.name)
 
         self.message = "helloHHH"
 
@@ -59,7 +62,25 @@ class letter_counter_app_tests(unittest.TestCase):
         self.test_output = lca.app_output(self.name, 
                                           self.letter,
                                           self.count_of_letters)
-        self.test_expected = "waseem, your message has 4 h's in it."
+        self.test_expected = "Waseeem, your message has 4 h's in it."
+        self.assertEqual(self.test_output, self.test_expected)
+    
+    # test 4
+    def test_4_spaces(self):
+        self.name = "waseem  h khalIFA"
+        self.name = lca.title_whitespace(self.name)
+
+        self.message = "helloHHH na name is 789   "
+
+        self.letter = "n"
+
+        self.count_of_letters = lca.count_letter_occurence(self.letter, 
+                                                           self.message)
+
+        self.test_output = lca.app_output(self.name, 
+                                          self.letter,
+                                          self.count_of_letters)
+        self.test_expected = "Waseem H Khalifa, your message has 2 n's in it."
         self.assertEqual(self.test_output, self.test_expected)
 
 
