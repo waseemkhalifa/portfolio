cce0ba56006b2ef43fb9a5d8c8350511c95a79a0
waseemkhalifa <whkhalifa@gmail.com>
2023-12-20 14:11:18 +0000

    added in country and language

--- Data Analysis/R/My IMDb Ratings/imdb_analysis/my_imdb_analysis.r
+++ Data Analysis/R/My IMDb Ratings/imdb_analysis/my_imdb_analysis.r
@@ -37,7 +37,10 @@ str(raw_dataset)
 
 dataset <- data.table(
   raw_dataset %>%
-    rename(runtime_min = runtime) %>%
+    rename(
+      runtime_min = runtime,
+      year_of_release = year
+      ) %>%
     mutate(
       rated_date = ymd(rated_date),
       box_office = as.double(gsub("\\$|,", '', box_office)),
@@ -76,6 +79,7 @@ decade <- data.table(
   dataset %>%
     mutate(decade = make_date(year_of_release,01,01)) %>%
     mutate(decade = floor_date(decade, years(10))) %>%
+    mutate(decade = year(decade)) %>%
     group_by(decade) %>%
     summarise(films = n_distinct(film_id),
               rating_avg = round(mean(my_rating),2),
@@ -91,8 +95,8 @@ decade <- data.table(
 #----------------------------------------------------
 director <- data.table(
   dataset %>%
-    filter(directors != "") %>%
-    group_by(directors) %>%
+    filter(director != "") %>%
+    group_by(director) %>%
     summarise(films = n_distinct(film_id),
               rating_avg = round(mean(my_rating),2),
               rating_median = median(my_rating),
@@ -109,12 +113,12 @@ director <- data.table(
 #----------------------------------------------------
 genre <- data.table(
   dataset %>%
-    filter(genres != "") %>%
-    select(film_id, title, genres, my_rating) %>%
-    mutate(genres = strsplit(as.character(genres), ", ")) %>% 
-    unnest(genres) %>%
+    filter(genre != "") %>%
+    select(film_id, title, genre, my_rating) %>%
+    mutate(genre = strsplit(as.character(genre), ", ")) %>% 
+    unnest(genre) %>%
     unique() %>%
-    group_by(genres) %>%
+    group_by(genre) %>%
     summarise(films = n_distinct(film_id),
               rating_avg = round(mean(my_rating),2),
               rating_median = median(my_rating),
@@ -137,33 +141,14 @@ rating <- data.table(
     arrange(-films)
 )
 
-
-#----------------------------------------------------
-# films by director
-#----------------------------------------------------
-director <- data.table(
-  dataset %>%
-    filter(directors != "") %>%
-    group_by(directors) %>%
-    summarise(films = n_distinct(film_id),
-              rating_avg = round(mean(my_rating),2),
-              rating_median = median(my_rating),
-              rating_mode = fn_mode(my_rating) 
-            ) %>%
-		mutate(percent = films / sum(films)) %>%
-    arrange(-films) %>%
-    filter(films >= 5)
-)
-
-
 #----------------------------------------------------
 # films by run time
 #----------------------------------------------------
 runtime_box <- data.table(
   dataset %>%
-    select(film_id, title, runtime_mins)
+    select(film_id, title, runtime_min)
 ) %>%
-  ggplot(aes(y = runtime_mins)) +
+  ggplot(aes(y = runtime_min)) +
 	geom_boxplot() +
 	ggtitle('Runtime (Minutes)') +
 	labs(y = 'Minutes') +
@@ -172,18 +157,58 @@ runtime_box <- data.table(
 
 runtime_hist <- data.table(
   dataset %>%
-    select(film_id, title, runtime_mins)
+    select(film_id, title, runtime_min)
 ) %>%
-  ggplot(aes(x = runtime_mins)) +
+  ggplot(aes(x = runtime_min)) +
 	geom_histogram(binwidth = 5, colour = 'black', fill = 'steelblue') + 
 	ggtitle('Runtime (Minutes)') +
 	labs(y = 'No. of Films', x = 'Minutes') +
-	scale_x_continuous(breaks = seq(0, 200, 5), lim = c(0, 200)) +
+	scale_x_continuous(breaks = seq(40, 200, 5), lim = c(40, 200)) +
 	# xlim(0, 200) +
 	scale_y_continuous(labels = scales::comma)
 
 
 
+#----------------------------------------------------
+# films by language
+#----------------------------------------------------
+language <- data.table(
+  dataset %>%
+    filter(language != "") %>%
+    select(film_id, title, language, my_rating) %>%
+    mutate(language = map(strsplit(language, split = ","), 1)) %>% 
+    unnest(language) %>%
+    unique() %>%
+    group_by(language) %>%
+    summarise(films = n_distinct(film_id),
+              rating_avg = round(mean(my_rating),2),
+              rating_median = median(my_rating),
+              rating_mode = fn_mode(my_rating) 
+            ) %>%
+		mutate(percent = films / sum(films)) %>%
+    arrange(-films) %>%
+    filter(films >= 3)
+)
 
 
+#----------------------------------------------------
+# films by country
+#----------------------------------------------------
+country <- data.table(
+  dataset %>%
+    filter(country != "") %>%
+    select(film_id, title, country, my_rating) %>%
+    mutate(country = map(strsplit(country, split = ","), 1)) %>% 
+    unnest(country) %>%
+    unique() %>%
+    group_by(country) %>%
+    summarise(films = n_distinct(film_id),
+              rating_avg = round(mean(my_rating),2),
+              rating_median = median(my_rating),
+              rating_mode = fn_mode(my_rating) 
+            ) %>%
+		mutate(percent = films / sum(films)) %>%
+    arrange(-films) %>%
+    filter(films >= 3)
+)
 
