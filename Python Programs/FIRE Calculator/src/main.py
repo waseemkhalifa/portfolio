63c27453bf90a53c6a0e3da45d65afc8cbed7869
waseemkhalifa <whkhalifa@gmail.com>
2024-03-04 09:26:17 +0000

    started writing out the pension compound functions

---
+++ Python Programs/FIRE Calculator/src/main.py
@@ -0,0 +1,45 @@
+# Notes
+# Have an option to show by currency
+
+def input_current_pension_holdings() -> str:
+    current_pension_holdings:str = input("Enter your current pension holdings: ")
+    
+    return current_pension_holdings
+
+
+def input_deposits() -> str:
+    deposit:str = input("Enter your monthly pension contributions: ")
+    
+    return deposit
+
+
+def input_current_age() -> str:
+    current_age:str = input("Enter your current age: ")
+    
+    return current_age
+
+
+def input_age_to_retire() -> str:
+    age_to_retire:str = input("Enter your retirement age: ")
+    
+    return age_to_retire
+
+
+
+# A = P(1 + r/12)^12t
+def compound_calculator():
+    
+
+
+
+
+deposit = input_deposits()
+print(deposit)
+
+
+
+
+
+
+
+
