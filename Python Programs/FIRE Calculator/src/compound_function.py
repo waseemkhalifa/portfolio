660e44651ecb92e10637bf7884f19f1be954a051
waseemkhalifa <whkhalifa@gmail.com>
2024-03-08 21:13:47 +0000

    added months_in_year to compound fn

--- Python Programs/FIRE Calculator/src/compound_function.py
+++ Python Programs/FIRE Calculator/src/compound_function.py
@@ -8,9 +8,11 @@ def compound_calculator(current_holdings:float,
 
     assumed_yearly_growth:float = assumed_yearly_growth / 100.0
 
-    compound_principle:float = current_holdings * (1.0 + assumed_yearly_growth / 12.0)**(12.0 * years_to_compound)
+    months_in_year:float = 12
 
-    future_value:float = deposit * (((1.0 + assumed_yearly_growth / 12.0)**(12.0 * years_to_compound) - 1.0) / (assumed_yearly_growth / 12))
+    compound_principle:float = current_holdings * (1.0 + assumed_yearly_growth / months_in_year)**(months_in_year * years_to_compound)
+
+    future_value:float = deposit * (((1.0 + assumed_yearly_growth / months_in_year)**(months_in_year * years_to_compound) - 1.0) / (assumed_yearly_growth / months_in_year))
 
     compounded_return:float = round(compound_principle + future_value, 2)
 
