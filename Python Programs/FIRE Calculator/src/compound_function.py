433f3f66b40fd30743d18a919968289b74a60df4
waseemkhalifa <whkhalifa@gmail.com>
2024-04-20 11:29:25 +0100

    cleaned up functions

--- Python Programs/FIRE Calculator/src/compound_function.py
+++ Python Programs/FIRE Calculator/src/compound_function.py
@@ -1,35 +1,10 @@
-import pandas as pd
-
 def compound_calculator(current_holdings:float,
                         deposit:float,
                         start_age:float,
                         end_age:float,
-                        assumed_yearly_growth:float) -> float:
-    
-    years_to_compound:float = end_age - start_age
+                        assumed_yearly_growth:float):
 
-    assumed_yearly_growth:float = assumed_yearly_growth / 100.0
-
-    months_in_year:float = 12
-
-    compound_principle:float = current_holdings * (1.0 + assumed_yearly_growth / months_in_year)**(months_in_year * years_to_compound)
-
-    future_value:float = deposit * (((1.0 + assumed_yearly_growth / months_in_year)**(months_in_year * years_to_compound) - 1.0) / (assumed_yearly_growth / months_in_year))
-
-    compounded_return:float = compound_principle + future_value
-
-    return compounded_return
-
-
-
-def compound_yearly_breakdown(current_holdings:float,
-                              deposit:float,
-                              start_age:float,
-                              end_age:float,
-                              assumed_yearly_growth:float,
-                              previous_return:float = None):
-
-    years_to_compound:float = end_age - start_age
+    years_to_compound:float = (end_age - start_age) + 1
 
     assumed_yearly_growth:float = assumed_yearly_growth / 100.0
 
@@ -41,38 +16,89 @@ def compound_yearly_breakdown(current_holdings:float,
                         "Compound Growth":[],
                         "Cummulative Compound Growth":[],
                         "Balance":[]}
-    
-    deposit_cumm_list:list = []
-    compound_growth_cumm_list:list = []
 
-    
     for year in range(years_to_compound):
 
-        previous_return = current_holdings if previous_return is None else compounded_return
+        if year == 0:
+
+            yearly_breakdown["Year"].append(year)
+            yearly_breakdown["Deposits"].append(current_holdings)
+            yearly_breakdown["Compound Growth"].append(0)
+            yearly_breakdown["Balance"].append(current_holdings)
+
+        else:
+            previous_return:float = current_holdings if year == 1 else compounded_return
+            
+            compound_principle:float = compound_principle_calc(current_holdings, 
+                                                               assumed_yearly_growth, 
+                                                               months_in_year, 
+                                                               year)
+            
+            future_value:float = future_value_calc(deposit,
+                                                   current_holdings, 
+                                                   assumed_yearly_growth, 
+                                                   months_in_year, 
+                                                   year)
+            
+            compounded_return:float = compound_principle + future_value
+
+            compound_growth:float = (compounded_return - previous_return) - (months_in_year * deposit)
+
+            yearly_breakdown["Year"].append(year)
+            yearly_breakdown["Deposits"].append(deposit*12)
+            yearly_breakdown["Compound Growth"].append(compound_growth)
+            yearly_breakdown["Balance"].append(compounded_return)
         
         year+=1
-        
-        compound_principle:float = current_holdings * (1.0 + assumed_yearly_growth / months_in_year)**(months_in_year * year)
-
-        future_value:float = deposit * (((1.0 + assumed_yearly_growth / months_in_year)**(months_in_year * year) - 1.0) / (assumed_yearly_growth / months_in_year))
-
-        compound_growth:float = ((compound_principle+future_value) - previous_return) - (months_in_year*deposit)
-
-        yearly_breakdown["Year"].append(year)
-        yearly_breakdown["Deposits"].append(deposit*12)
-        yearly_breakdown["Compound Growth"].append(compound_growth)
-        yearly_breakdown["Balance"].append(compound_principle + future_value)
-
-        compounded_return:float = compound_principle + future_value
 
     return compounded_return, yearly_breakdown
 
-compounded_return = compound_yearly_breakdown(25000,
+
+
+def compound_principle_calc(current_holdings:float,
+                            assumed_yearly_growth:float,
+                            months_in_year:float,
+                            year:int):
+    
+    ch = current_holdings
+    ayg = assumed_yearly_growth
+    miy = months_in_year
+    
+    compound_principle:float = (1.0 + ayg / miy)**(miy * year)
+    compound_principle = compound_principle * ch
+
+    return compound_principle
+
+
+
+def future_value_calc(deposit:float,
+                      current_holdings:float,
+                      assumed_yearly_growth:float,
+                      months_in_year:float,
+                      year:int):
+    
+    dep = deposit
+    ch = current_holdings
+    ayg = assumed_yearly_growth
+    miy = months_in_year
+    
+    future_value:float = ((1.0 + ayg / miy)**(miy * year) - 1.0)
+    future_value:float = future_value / (ayg / miy)
+    future_value = future_value * dep
+
+    return future_value
+
+
+
+
+
+
+compounded_return = compound_calculator(25000,
                               2500,
                               34,
                               52,
                               5)
 
-print(compounded_return)
+print(compounded_return[1])
 
 
