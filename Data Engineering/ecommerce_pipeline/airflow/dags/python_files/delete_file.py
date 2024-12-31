0efc379bc83a39f06df698db1fd2f7ba5651c6bf
waseemkhalifa <whkhalifa@gmail.com>
2024-12-31 20:13:34 +0000

    added function documentation

--- Data Engineering/ecommerce_pipeline/airflow/dags/python_files/delete_file.py
+++ Data Engineering/ecommerce_pipeline/airflow/dags/python_files/delete_file.py
@@ -3,4 +3,7 @@ import os
 
 ## ------------ Functions ------------ ##
 def delete_file(file_name):
+    """
+    Deletes a single file specified by the filename/path
+    """
     os.remove(file_name) 
