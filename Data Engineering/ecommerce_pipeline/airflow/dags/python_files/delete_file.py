33534895157f366cd3d1110d6dc5f51df1e4ece3
waseemkhalifa <whkhalifa@gmail.com>
2025-01-02 10:29:53 +0000

    added try except

--- Data Engineering/ecommerce_pipeline/airflow/dags/python_files/delete_file.py
+++ Data Engineering/ecommerce_pipeline/airflow/dags/python_files/delete_file.py
@@ -6,4 +6,8 @@ def delete_file(file_name):
     """
     Deletes a single file specified by the filename/path
     """
-    os.remove(file_name) 
+    try:
+        os.remove(file_name)
+    except OSError as error:
+        print(error)
+        print(f"{file_name} not found")
