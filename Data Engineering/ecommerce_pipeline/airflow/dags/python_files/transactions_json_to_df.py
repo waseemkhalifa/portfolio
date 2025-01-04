6a2971ad3a9bea5d9bf20942f8caa22ddd4ec1ef
waseemkhalifa <whkhalifa@gmail.com>
2025-01-04 10:15:49 +0000

    fixed json issue

--- Data Engineering/ecommerce_pipeline/airflow/dags/python_files/transactions_json_to_df.py
+++ Data Engineering/ecommerce_pipeline/airflow/dags/python_files/transactions_json_to_df.py
@@ -6,21 +6,24 @@ import pandas as pd
 DF_COLUMNS = ["transaction_id", "transaction_datetime", "transaction_data"]
 
 
-
 ## ------------ Functions ------------ ##
-def transactions_json_to_dataframe(lst, df_columns=DF_COLUMNS) -> pd.DataFrame:
+def transactions_json_to_dataframe(lst,
+                                   df_columns=DF_COLUMNS) -> pd.DataFrame:
     """
-    Takes list of json objects and converts them into a DataFrame
+    Takes list of json objects and returns a DataFrame
     """
     
     df = pd.DataFrame(columns=df_columns)
 
     for json in lst:
-        # List of new values
-        temp_df = [json[0][0]["transaction_id"], json[0][0]["transaction_datetime"], json[0][0]]
-        # New data as pandas.DataFrame
-        temp_df = pd.DataFrame(columns=df.columns, data=[temp_df])
-        # Overwrite original dataframe
-        df = pd.concat([df, temp_df], axis=0)
+        for transaction in json:
+            # List of new values
+            temp_df = [json[transaction][0]["transaction_id"], 
+                       json[transaction][0]["transaction_datetime"], 
+                       json[transaction][0]]
+            # New data as pandas.DataFrame
+            temp_df = pd.DataFrame(columns=df.columns, data=[temp_df])
+            # Overwrite original dataframe
+            df = pd.concat([df, temp_df], axis=0)
     
     return df
